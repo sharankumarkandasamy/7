54351a51cb035c7a404b490eb0ed900448f7a1eb
sharankumarkandasamy <sharankumar29.2@gmail.com>
2016-09-08 11:27:19 +0530

    Add files via upload

---
+++ Factorial.java
@@ -0,0 +1,18 @@
+package guvi;
+
+import java.util.Scanner;
+
+public class Factorial 
+{
+	public static void main(String[] args) 
+	{
+		int fact=1;
+		//int number=5;
+		for(int i=1;i<=10;i++)
+		{
+			fact*=i;
+		}
+			System.out.println("The factorial of "+ 10 +" is:"+fact);
+		
+	}
+}
